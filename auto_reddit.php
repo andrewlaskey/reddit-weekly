eb8bdb8a7811f6e39989f3b8af76628f06419a73
andrewlaskey <andrew.laskey@gmail.com>
2014-02-20 13:42:46 -0800

    update to get sub reedits from db

--- auto_reddit.php
+++ auto_reddit.php
@@ -12,55 +12,6 @@
 
 	if ($sendmail) {
 
-		//Compile the list
-		if ($title == "news") {
-			$submissions = array();
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/Foodforthought/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/news/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/politics/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/science/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/technology/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/TrueReddit/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/worldnews/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-		} else {
-			$submissions = array();
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/alltheleft/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/cooperatives/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/economicdemocracy/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/GreenParty/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/labor/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/Liberal/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-
-			$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/SocialDemocracy/top/.json?limit=2&t=week"));
-			$submissions = array_merge($submissions, $reddit_data->data->children);
-		}
-
 		//Get the list of subscribers and ship out
 		require("db_info.php");
 
@@ -74,6 +25,25 @@
 
 			if ($db_selected) {
 
+				//Compile the list
+				$submissions = array();
+
+				$sql = sprintf("SELECT * FROM topics WHERE topic = '%s'",
+					mysql_real_escape_string($title));
+
+				$result = mysql_query($sql);
+
+				if ($result) {
+
+					while($row = mysql_fetch_array($result))
+						{
+							$reddit_data = json_decode(file_get_contents("http://www.reddit.com/r/" . $row["reddit"] . "/top/.json?limit=2&t=week"));
+							$submissions = array_merge($submissions, $reddit_data->data->children);
+						}
+
+				}
+
+				//Get Subscriber List and mail out
 				$sql = sprintf("SELECT * FROM email_list WHERE %s = 1",
 					mysql_real_escape_string($title));
 
@@ -93,15 +63,17 @@
 				  		foreach ($submissions as $link) {
 							$message.= "<p>" . $link->data->title . "<br />";
 							$message.= $link->data->url . "</p>";
-							echo $link->data->url;
 						}
 
 				  		mail($to,$subject,$message,$headers);
 				  	}
+
 				}//End sql result
 
 			}//End db select
+
 		}//End connection check
+
 	}//End Send Mail
 
 
